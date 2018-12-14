5eb99a1a79003261107fba3282c3b59404e61200
Peter Hozak <peter.hozak@gmail.com>
2018-12-14 20:51:05 +0100

    Updates

---
+++ precache-manifest.f5b75e426c60c000cdd7133d37a101b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8384bf931b27058cd07e",
+    "url": "/data_viewer/static/js/runtime~main.8384bf93.js"
+  },
+  {
+    "revision": "c300bea143ffa2612b26",
+    "url": "/data_viewer/static/js/main.c300bea1.chunk.js"
+  },
+  {
+    "revision": "b73788dd894e584e7b5c",
+    "url": "/data_viewer/static/js/1.b73788dd.chunk.js"
+  },
+  {
+    "revision": "c300bea143ffa2612b26",
+    "url": "/data_viewer/static/css/main.0d65be5c.chunk.css"
+  },
+  {
+    "revision": "44aef8b993de934204d32e2d4ccc5c5d",
+    "url": "/data_viewer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/data_viewer/precache-manifest.c20ef976dfa1d9edad99b6ca7a3700fd.js"
+  "/data_viewer/precache-manifest.f5b75e426c60c000cdd7133d37a101b9.js"
 );
 
 workbox.clientsClaim();
